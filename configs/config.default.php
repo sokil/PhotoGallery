7c9338eb390bde7c0cbca7ac7d39e1361c32063c
Dmytro Sokil <dmytro.sokil@gmail.com>
2016-03-23 00:09:14 +0200

    def config

---
+++ configs/config.default.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'imageList' => [
+        'pageLength' => 5,
+    ],
+    'public' => true,
+    'socialButtons' => true,
+    'title' => 'Site title',
+    'contactEmail' => 'info@server.com',
+];
